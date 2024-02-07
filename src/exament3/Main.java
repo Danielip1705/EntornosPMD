6e972b80338886bb9adfd18f81ba700170246f17
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-02-07 19:20:34 +0100

    PMD parchis terminado, main todavia le falta

--- src/exament3/Main.java
+++ src/exament3/Main.java
@@ -2,47 +2,65 @@ package exament3;
 
 import java.util.Scanner;
 
+/***
+ * Clase main donde se va a ejecutar el programa
+ */
 public class Main {
 
+	/***
+	 * Metodo main donde permite ejecutar el programa
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner leer = new Scanner(System.in);
 		Parchis parchis;
 		String nomJ1, nomJ2;
 		String ganador = "";
 		int turno = 1;
 
-		System.out.println("BIENVENIDO AL PARCHÕçS DEL IES NERVI”N =D");
+		System.out.println("BIENVENIDO AL PARCHÕçS DEL IES NERVIÔøΩN =D");
 
 		System.out.println("Introduzca el nombre del jugador 1");
-		nomJ1 = sc.nextLine();
+		nomJ1 = leer.nextLine();
 
 		do {
 			System.out.println("Introduzca el nombre del jugador 2. Tiene que ser distinto al del jugador 1.");
-			nomJ2 = sc.nextLine();
+			nomJ2 = leer.nextLine();
 		} while (nomJ2.equals(nomJ1));
 
 		parchis = new Parchis(nomJ1, nomJ2);
 
 		ganador = parchis.esGanador();
-		while (ganador.equals("") == true) {
+		while (ganador.equals("")) {
 			System.out.println("Pulse una tecla para avanzar");
-			sc.nextLine();
+			leer.nextLine();
 
 			turno(parchis, turno);
-			if(Parchis.dado1 != Parchis.dado2) {
-				turno = turno==1 ? 2 : 1;
+			if (Parchis.dado1 != Parchis.dado2) {
+				turno = turno == 1 ? 2 : 1;
 			}
 
 			ganador = parchis.esGanador();
 		}
 
-		System.out.println("°ENHORABUENA " + parchis.nomJ1 + ", HAS GANADO!");
+		System.out.println("ÔøΩENHORABUENA " + parchis.nomJ1 + ", HAS GANADO!");
+
+		leer.close();
 
 	}
 
+	/***
+	 * Creamos funcion turno en el cula nos muestra que jugador le toca y los
+	 * numeros que le han salido en los dados
+	 * 
+	 * @param parchis El objeto parchis de la clase parchis que permite sacar los
+	 *                dados
+	 * @param jugador Un numero entero que muestra la posicion del jugador
+	 */
 	public static void turno(Parchis parchis, int jugador) {
 
-		System.out.println("Turno de " + (jugador==1 ? parchis.nomJ1 : parchis.nomJ2));
+		System.out.println("Turno de " + (jugador == 1 ? parchis.nomJ1 : parchis.nomJ2));
 		Parchis.tiraDados();
 		System.out.println("La tirada ha sido " + Parchis.dado1 + " y " + Parchis.dado2);
 		parchis.avanzaPosiciones(jugador);
--- src/exament3/Parchis.java
+++ src/exament3/Parchis.java
@@ -2,31 +2,79 @@ package exament3;
 
 import java.util.Random;
 
+/***
+ * Clase donde se creara las funciones para el juego parchis
+ */
 public class Parchis {
+	/***
+	 * Constante donde guarda el tamaño del tablero
+	 */
 	public static final int TAM_TABLERO = 10;
 
-	static int dado1, dado2;
+	/***
+	 * Variable entera que guarda un numero
+	 */
+	//public
+	public static int dado1;
 
-	int fichaJ1=0, fichaJ2=0;
+	/***
+	 * Variable entera que guarda un numero
+	 */
+	public static int dado2;
 
-	String nomJ1="", nomJ2="";
+	/***
+	 * Variable entera que guarda la posicion del jugador1
+	 */
+	public int fichaJ1;
 
+	/***
+	 * Variable entera que guarda la posicion del jugador2
+	 */
+	public int fichaJ2;
+
+	/***
+	 * Cadena que guarda el nombre del jugador 1
+	 */
+	public String nomJ1 = "";
+
+	/***
+	 * Cadena que guarda el nombre del jugador 2
+	 */
+	public String nomJ2 = "";
+
+	/***
+	 * Constructor sin parametros
+	 */
 	public Parchis() {
 		super();
 	}
 
+	/***
+	 * Constructor con parametros
+	 * 
+	 * @param nomJ1 Cadena que guarda el nombre del jugador 1
+	 * @param nomJ2 Cadena que guarda el nombre del jugador 2
+	 */
 	public Parchis(String nomJ1, String nomJ2) {
 		super();
 		this.nomJ1 = nomJ1;
 		this.nomJ2 = nomJ2;
 	}
 
+	/***
+	 * Funcion estatica que le da valores aleatorios del 1 al 6 a las variable
+	 * enteras dado1 y dado2
+	 */
 	public static void tiraDados() {
 		Random rand = new Random();
 		dado1 = rand.nextInt(1, 7);
 		dado2 = rand.nextInt(1, 7);
 	}
 
+	/***
+	 * Funcion que pinta el tablero y los jugadores segun su tamaño con la constante
+	 * TAM_TABLERO
+	 */
 	public void pintaTablero() {
 		System.out.print("\tI");
 		for (int i = 1; i < TAM_TABLERO; i++) {
@@ -45,7 +93,6 @@ public class Parchis {
 		}
 
 		System.out.println("\tF");
-		
 
 		System.out.print(nomJ2 + "\tI");
 
@@ -58,9 +105,14 @@ public class Parchis {
 		}
 		System.out.println("\tF\n");
 
-
 	}
 
+	/***
+	 * Funcion que mueve al jugador en el tablero creado segun los dados que le ha
+	 * tocado
+	 * 
+	 * @param jugador Numero entero que indica el jugador 1 o 2
+	 */
 	public void avanzaPosiciones(int jugador) {
 		int tirada = dado1 + dado2;
 
@@ -79,6 +131,9 @@ public class Parchis {
 		}
 	}
 
+	/***
+	 * Funcion que imprime si jugador1 o jugador2 va ganando o si van empatados
+	 */
 	public void estadoCarrera() {
 		if (fichaJ1 > fichaJ2) {
 			System.out.println("Va ganando " + nomJ1);
@@ -90,8 +145,13 @@ public class Parchis {
 		System.out.println();
 	}
 
+	/***
+	 * Funcion que imprime el nombre del jugador que ha ganado
+	 * 
+	 * @return Nombre del jugador o cadena vacia
+	 */
 	public String esGanador() {
-		
+
 		if (fichaJ1 == TAM_TABLERO) {
 			return nomJ1;
 		} else if (fichaJ2 == TAM_TABLERO) {
